58877742289837a331ed913a6e769bda785824e3
sii15175 <ceztro@gmail.com>
2024-09-07 13:20:54 +0200

    temporarily commented out the DB lifecycle

--- modules/rds/main.tf
+++ modules/rds/main.tf
@@ -14,9 +14,9 @@ resource "aws_db_instance" "rds" {
   db_subnet_group_name            = aws_db_subnet_group.rds_subnet_group.name
   skip_final_snapshot             = true
 
-  lifecycle {
-    prevent_destroy = true
-  }
+  # lifecycle {
+  #   prevent_destroy = true
+  # }
 
   tags = merge(
     { "Env" = var.env, "Name" = "${var.project_name}-rds" },
