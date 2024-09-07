8184c37d23ff7227f325500b898ddc1f9eba4833
sii15175 <ceztro@gmail.com>
2024-09-07 13:03:42 +0200

    fixed thumbprint and url for oidc

--- modules/kubernetes/main.tf
+++ modules/kubernetes/main.tf
@@ -39,8 +39,8 @@ resource "aws_eks_cluster" "this" {
 
 resource "aws_iam_openid_connect_provider" "this" {
   client_id_list  = ["sts.amazonaws.com"]
-  thumbprint_list = [data.aws_eks_cluster.this.identity.oidc.issuer.thumbprint] 
-  url             = data.aws_eks_cluster.this.identity.oidc.issuer              
+  thumbprint_list = [data.aws_eks_cluster.this.identity.0.oidc.0.thumbprint] 
+  url             = data.aws_eks_cluster.this.identity.0.oidc.0.issuer           
 }
 
 
