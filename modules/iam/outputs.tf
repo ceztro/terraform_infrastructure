de29a8ec72e9e84d92957a3ac9ecb936099f82d4
👋 Cezary Trojanowski <57718036+ceztro@users.noreply.github.com>
2024-09-13 13:15:35 +0200

    cleaned up the repository from the resources I will not use anymore (#15)

--- modules/iam/outputs.tf
+++ modules/iam/outputs.tf
@@ -5,17 +5,3 @@ output "kms_eks_key_arn" {
 output "usernames" {
   value = [for user in aws_iam_user.eks_admins : user.name]
 }
-
-##################
-## Github Actions IAM User Access Keys
-##################
-
-# output "github_actions_access_key_id" {
-#   value = aws_iam_access_key.github_actions.id
-# }
-
-# output "github_actions_secret_access_key" {
-#   value       = aws_iam_access_key.github_actions.secret
-#   description = "This is the secret access key for the GitHub Actions IAM user"
-#   sensitive   = true
-# }
